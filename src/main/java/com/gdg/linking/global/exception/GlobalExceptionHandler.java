3a6cc7f133bf0e31320f03ed0824a7048a8820c6
youngsun <sun010331@naver.com>
2026-01-14 19:45:49 +0900

    fix : swagger 에러 해결
    - 전역 처리 코드에 @Hidden 추가

--- src/main/java/com/gdg/linking/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gdg/linking/global/exception/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.gdg.linking.global.exception;
 import com.gdg.linking.global.exception.custom.BadRequestException;
 import com.gdg.linking.global.exception.custom.NotFoundException;
 import com.gdg.linking.global.exception.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Hidden
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
