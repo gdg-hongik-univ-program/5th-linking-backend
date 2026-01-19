b95d33359261243281d9afb6837277142faf9d77
최승원 <choeseung-won@choeseung-won-ui-MacBookPro.local>
2026-01-19 18:16:08 +0900

    feat: 폴더 내 하위 폴더 생성 기능 추가 및 폴더 삭제 시 하위 폴더 및 아이템 삭제 기능 추가

--- src/main/java/com/gdg/linking/domain/folder/Folder.java
+++ src/main/java/com/gdg/linking/domain/folder/Folder.java
@@ -1,8 +1,14 @@
 package com.gdg.linking.domain.folder;
 
-import com.gdg.linking.domain.user.User; // 기존 User 엔티티 경로 확인 필요
+import com.gdg.linking.domain.item.Item;
+import com.gdg.linking.domain.user.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,13 +20,30 @@ public class Folder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "f_id") // ERD의 f_id 매핑
+    @Column(name = "f_id")
     private Long fId;
 
-    @Column(name = "f_name", nullable = false, length = 45) // ERD의 f_name
+    @Column(name = "f_name", nullable = false, length = 45)
     private String folderName;
 
     @ManyToOne(fetch = FetchType.LAZY) // 여러 개의 폴더가 한 명의 사용자에게 속함, 지연 로딩 권장
     @JoinColumn(name = "user_id") // 외래키 user_id
     private User user;
+
+    // 계층형 폴더 기능(폴더 내 폴더)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Folder parentFolder;
+
+    // 하위 폴더 연쇄 삭제
+    @OneToMany(mappedBy = "parentFolder", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Builder.Default
+    private List<Folder> childFolders = new ArrayList<>();
+
+    // 폴더 내 Item 연쇄 삭제 설정
+    @OneToMany(mappedBy = "folder", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @Builder.Default
+    private List<Item> items = new ArrayList<>();
 }
