8bc682eb1352aeb8c759721dc919ee6c2479c7be
youngsun <sun010331@naver.com>
2026-01-25 01:09:59 +0900

    fix

--- src/main/java/com/gdg/linking/global/config/WebConfig.java
+++ src/main/java/com/gdg/linking/global/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 형식상 패턴 허용
+                .allowedOrigins("https://www.thelinking.store")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 3. 필요한 메서드만 허용
                 .allowCredentials(true) // 4. 쿠키/인증 헤더 허용 시 필수
                 .maxAge(3600); // 5. Preflight 요청 캐싱 시간 설정 (성능 향상)
