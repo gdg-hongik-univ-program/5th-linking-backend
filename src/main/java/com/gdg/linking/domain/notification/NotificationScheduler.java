8db82d1f0f23e232754c6c5d0bfcd7f0d3590f6d
최승원 <choeseung-won@choeseung-won-ui-MacBookPro.local>
2026-02-02 11:57:56 +0900

    알림 테스트

--- src/main/java/com/gdg/linking/domain/notification/NotificationScheduler.java
+++ src/main/java/com/gdg/linking/domain/notification/NotificationScheduler.java
@@ -14,7 +14,7 @@ public class NotificationScheduler {
 
     // 매일 아침 8시에 실행되도록 설정
     //@Scheduled(cron = "0 0 8 * * *", zone = "Asia/Seoul")
-    @Scheduled(cron = "0 50 11 * * *")
+    @Scheduled(cron = "0 03 12 * * *", zone = "Asia/Seoul")
     public void runDeadlineCheck() {
         log.info("마감일 알림 체크 스케줄러 시작 - 아침 8시");
         notificationService.createDeadlineNotifications();
